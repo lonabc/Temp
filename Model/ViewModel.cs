2de0054c4969487d0e0125c5363abe60eb410016
行将至 <114801169+lonabc@users.noreply.github.com>
2025-03-15 22:16:50 +0800

    livechart兼容

---
+++ Model/ViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.Measure;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Drawing.Geometries;
+using LiveChartsCore.SkiaSharpView.Extensions;
+using LiveChartsCore.SkiaSharpView.WPF;
+
+namespace WpfSample // livecharts 折线图数据设置
+{
+    public class ViewModels
+    {
+        public ISeries[] Series { get; set; }
+            = new ISeries[]
+            {
+                new LineSeries<double>
+                {
+                    Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+                    Fill = null
+                }
+            };
+        public ObservableCollection<Axis> XAxes { get; set; }
+        public ObservableCollection<Axis> YAxes { get; set; }
+        public ObservableCollection<double> XLables { get; set; }
+        public ViewModels()
+        {
+             XAxes = new ObservableCollection<Axis>
+            {
+                new Axis
+                {
+                    Name = "X轴",
+                 //   Labels = new List<string> { "A", "B", "C", "D", "E" },
+                    TextSize = 12,
+                    NameTextSize = 14,
+                    NamePadding = new LiveChartsCore.Drawing.Padding(0, 10),
+                    LabelsRotation = 45 //标签斜45度
+                }
+            };
+            XLables = new ObservableCollection<double> { 1, 2, 3, 4, 5, 6, 7 };
+
+             YAxes = new ObservableCollection<Axis>
+            {
+                new Axis
+                {
+                    Name = "Y轴",
+                    TextSize = 12,
+                    NameTextSize = 14,
+                    NamePadding = new LiveChartsCore.Drawing.Padding(0, 10),
+                    MinLimit = 0,
+                    MaxLimit = 10
+                }
+            };
+        }
+    }
+}
--- index.xaml.cs
+++ index.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,17 +13,23 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WpfSample;
 
 namespace WpfAppLogin
 {
     /// <summary>
     /// index.xaml 的交互逻辑
     /// </summary>
+    /// 
+    
     public partial class index 
     {
+
         public index()
         {
             InitializeComponent();
+            this.DataContext = new ViewModels(); //绑定ViewModel数据源
         }
     }
 }
+
