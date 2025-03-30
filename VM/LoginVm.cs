dde781799f12cbd1f07a90445790b9a7f7d68394
行将至 <114801169+lonabc@users.noreply.github.com>
2025-03-30 12:47:04 +0800

    Update LoginVm.cs

--- VM/LoginVm.cs
+++ VM/LoginVm.cs
@@ -1,28 +1,40 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Views;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Security.RightsManagement;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using WpfAppLogin.Model;
+
 using WpfAppLogin.RelayCommond;
 
 namespace WpfAppLogin.VM
 {
-    class LoginVm:INotifyPropertyChanged
+    public class LoginVm : INotifyPropertyChanged
     {
         #region 服务层数据绑定
         private LoginModel _loginModel;
 
-        public LoginVm()
+        private readonly HttpClient _httpClient;
+   
+
+        public LoginVm(IHttpClientFactory httpClientFactory,LoginModel loginModel)
         {
-            _loginModel = new LoginModel();
+            _httpClient = httpClientFactory.CreateClient("MyApi");
+            _loginModel = loginModel;
+           
         }
       
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler PropertyChanged; 
         private void RaisePropertyChanged(string propertyName)
         {
             if (PropertyChanged != null)
@@ -43,32 +55,47 @@ namespace WpfAppLogin.VM
         }
         #endregion
 
-        public void login()
+        public async Task<string> login()
         {
-            Console.WriteLine(name+""+password);
-            if (_loginModel.name == "admin" && _loginModel.password == "admin")
+
+            try
             {
-                index index = new index();
-                index.Show();
-                MessageBox.Show(name+""+password);
+                _loginModel.id = "1";
+                var json = JsonSerializer.Serialize(_loginModel);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var fullUrl = new Uri(_httpClient.BaseAddress, "HomeController/Login/Login").AbsoluteUri;
+                Debug.WriteLine($"请求URL: {fullUrl}");
+                Debug.WriteLine($"请求内容: {json}");
+
+                var response = await _httpClient.PostAsync("HomeController/Login/Login", content);
+                response.EnsureSuccessStatusCode();
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return "200";
+                //       MessageBox.Show(responseBody);
             }
-            else
+            catch (HttpRequestException ex)
             {
-                Console.WriteLine("Login Failed");
-                
+                Trace.WriteLine($"请求失败: {ex.Message}");
+                if (ex.StatusCode.HasValue)
+                {
+                    Trace.WriteLine($"状态码: {ex.StatusCode}");
+                }
+                return ex.StatusCode.ToString();
             }
+         
+
         }
 
         bool CanLoginExecute()
         {
             return true;
          }
-        public ICommand LoginActoin
-        {
-            get
-            {
-                return new Commond(login,CanLoginExecute);
-            }
-        }
+        //public ICommand LoginActoin
+        //{
+        //    get
+        //    {
+        //        return new Commond(login,CanLoginExecute);
+        //    }
+        //}
     }
 }
